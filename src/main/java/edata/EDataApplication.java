c081e9339db54cecf9cf1921c13715d0266202cf
gentjan kolicaj <gentjankolicaj@gmail.com>
2018-11-15 17:50:26 +0100

    Initial commit

---
+++ src/main/java/edata/EDataApplication.java
@@ -0,0 +1,14 @@
+package edata;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDataApplication {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(EDataApplication.class,args);
+    }
+}
