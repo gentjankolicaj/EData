9fdb2a1d12832b69059ca4d45e984f109d0eb66f
gentjan kolicaj <gentjankolicaj@gmail.com>
2018-11-16 17:09:35 +0100

    Added to classes to be used as components for helping with different bootstrap.

---
+++ src/main/java/edata/bootstrap/DataBoostrap.java
@@ -0,0 +1,12 @@
+package edata.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBoostrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/main/java/edata/bootstrap/GeneralBootstrap.java
@@ -0,0 +1,13 @@
+package edata.bootstrap;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GeneralBootstrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
