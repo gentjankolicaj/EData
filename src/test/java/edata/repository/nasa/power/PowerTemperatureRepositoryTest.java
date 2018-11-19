d6117028ec93c7c282180256e3e11c2e12ac0d5f
gentjan kolicaj <gentjankolicaj@gmail.com>
2018-11-19 12:05:28 +0100

    Classes for repository testing added.Not implemented yet !!!.

---
+++ src/test/java/edata/repository/nasa/power/PowerTemperatureRepositoryTest.java
@@ -0,0 +1,44 @@
+package edata.repository.nasa.power;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PowerTemperatureRepositoryTest {
+
+    @Test
+    @Rollback
+    public void create() throws Exception{
+    }
+
+    @Test
+    public void createAll() throws Exception{
+    }
+
+    @Test
+    public void readById()throws Exception {
+    }
+
+    @Test
+    public void readAll() throws Exception{
+    }
+
+    @Test
+    public void update() throws Exception{
+    }
+
+    @Test
+    public void delete() throws Exception{
+    }
+
+    @Test
+    @Rollback
+    public void deleteAll() throws Exception{
+    }
+}
