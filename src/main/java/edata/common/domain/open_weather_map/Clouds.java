7ea48b77cd32c3e4e9914b1adeaa72580203e018
gentjan kolicaj <gentjankolicaj@gmail.com>
2018-11-26 10:26:27 +0100

    New domain classes for OpenWeatherMap api.

---
+++ src/main/java/edata/common/domain/open_weather_map/Clouds.java
@@ -0,0 +1,15 @@
+package edata.common.domain.open_weather_map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Clouds {
+
+    private Float all;
+}
---
+++ src/main/java/edata/common/domain/open_weather_map/Sys.java
@@ -0,0 +1,20 @@
+package edata.common.domain.open_weather_map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sys {
+
+    private Long type;
+    private Long id;
+    private Float message;
+    private String country;
+    private Long sunrise;
+    private Long sunset;
+}
